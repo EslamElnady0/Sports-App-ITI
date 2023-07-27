9def3263182e958a5dd1767b91f0fe7be416ca73
EslamElnady0 <eslamelnady254@gmail.com>
2023-07-27 10:13:13 +0300

    teams and top scorers logic

---
+++ lib/Data/Cubits/Top Scorers/top_scorers_state.dart
@@ -0,0 +1,15 @@
+part of 'top_scorers_cubit.dart';
+
+@immutable
+abstract class TopScorersState {}
+
+class TopScorersInitial extends TopScorersState {}
+
+class TopScorersLoading extends TopScorersState {}
+
+class TopScorersSuccess extends TopScorersState {
+  final TopScorers response;
+  TopScorersSuccess({required this.response});
+}
+
+class TopScorersFailure extends TopScorersState {}
